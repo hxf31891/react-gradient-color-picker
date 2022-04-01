e448ae460ccdb115020499a8c1b009a90687f182
Harry <hxfox1@gmail.com>
2022-04-01 01:09:50 -0500

    scroll bug

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React, { useRef, useState, useEffect } from "react"
-import PickerContextWrapper from './context'
-import { useWindowSizes } from 'react-use-window-sizes'
-import Picker from "./Picker"
-import './styles.css'
+import { useScrollPosition } from 'react-use-scroll-position';
+import { useWindowSizes } from 'react-use-window-sizes';
+import PickerContextWrapper from './context';
+import Picker from "./Picker";
+import './styles.css';
 
 
 function ColorPicker({ value = 'rgba(175, 51, 242, 1)', onChange = () => {}, hideControls, hideInputs, hidePresets, presets }) {
-  const contRef = useRef(null)
-  const { width, height } = useWindowSizes()
-  const [bounds, setBounds] = useState({})
+  const contRef = useRef(null);
+  const { width, height } = useWindowSizes();
+  const [bounds, setBounds] = useState({});
+  const { x, y } = useScrollPosition();
 
   useEffect(() => {
     setBounds(contRef?.current?.getBoundingClientRect())
     setTimeout(() => setBounds(contRef?.current?.getBoundingClientRect()), 150)
-  }, [width, height])
+  }, [width, height, x, y])
 
   return (
     <div ref={contRef}>
