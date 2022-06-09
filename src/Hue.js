5d78753b75d59b79942475cd5ead146ca76d6cfa
Harry <hxfox1@gmail.com>
2022-06-09 03:08:25 -0500

    hue handle misaligned

--- src/Hue.js
+++ src/Hue.js
@@ -1,43 +1,57 @@
-import React, { useRef, useState } from 'react'
-import { usePicker } from "./context"
-import usePaintHue from './usePaintHue'
+import React, { useRef, useState } from "react";
+import { usePicker } from "./context";
+import usePaintHue from "./usePaintHue";
 
 const Hue = () => {
   const barRef = useRef(null);
   const { handleHue, internalHue } = usePicker();
-  const [dragging, setDragging] = useState(false)
+  const [dragging, setDragging] = useState(false);
   usePaintHue(barRef);
 
   const stopDragging = () => {
-    setDragging(false)
-  }
+    setDragging(false);
+  };
 
   const handleDown = () => {
-    setDragging(true)
-  }
+    setDragging(true);
+  };
 
-  const handleMove = (e) => {
+  const handleMove = e => {
     if (dragging) {
-      handleHue(e)
+      handleHue(e);
     }
-  }
+  };
 
-  const handleClick = (e) => {
+  const handleClick = e => {
     if (!dragging) {
-      handleHue(e)
+      handleHue(e);
     }
-  }
+  };
 
   return (
-    <div className='bar-wrap' onMouseEnter={stopDragging} onMouseLeave={stopDragging}>
-      <div className='ps-rl bar-wrap-inner' onMouseUp={stopDragging}>
-        <div className='c-resize ps-rl' onMouseMove={(e) => handleMove(e)}>
-          <div style={{left: internalHue * .766666666666667, top: -.5}} className='handle' onMouseDown={handleDown} />
-          <canvas ref={barRef} width='294px' height='14px' style={{position: 'relative', borderRadius: 14}} onClick={(e) => handleClick(e)} />
+    <div
+      className="bar-wrap"
+      onMouseEnter={stopDragging}
+      onMouseLeave={stopDragging}
+    >
+      <div className="ps-rl bar-wrap-inner" onMouseUp={stopDragging}>
+        <div className="c-resize ps-rl" onMouseMove={e => handleMove(e)}>
+          <div
+            style={{ left: internalHue * 0.766666666666667, top: 2 }}
+            className="handle"
+            onMouseDown={handleDown}
+          />
+          <canvas
+            ref={barRef}
+            width="294px"
+            height="14px"
+            style={{ position: "relative", borderRadius: 14 }}
+            onClick={e => handleClick(e)}
+          />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Hue;
