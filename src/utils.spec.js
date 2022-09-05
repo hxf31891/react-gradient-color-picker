2e303f541b88a61d3e0469ef6cd584809107bdc2
Thiago Jourdan <thiagojourdan@gmail.com>
2022-09-05 15:43:40 -0300

    :umbrella: Add tests for "getNewHsl" utility

--- src/utils.spec.js
+++ src/utils.spec.js
@@ -1,4 +1,4 @@
-import {isUpperCase} from './utils'
+import {isUpperCase, getNewHsl} from './utils'
 
 describe('isUpperCase', () => {
   it('should return true when the first letter of the string is upper-cased', () => {
@@ -7,5 +7,21 @@ describe('isUpperCase', () => {
 
   it('should return false when the first letter of the string is lower-cased', () => {
     expect(isUpperCase('aLOHA OE')).toBe(false)
+  })
+})
+
+describe('getNewHsl', () => {
+  it('should return correct RGBA color for given HSL color', () => {
+    const callback = () => {};
+    const output = getNewHsl(116, 79, 19, 0.5, callback)
+
+    expect(output).toEqual('rgba(15, 87, 10, 0.5)')
+  })
+
+  it('should trigger callback with correct arguments', () => {
+    const callback = jest.fn();
+    getNewHsl(116, 79, 19, 0.5, callback)
+
+    expect(callback).toHaveBeenCalledWith(116)
   })
 })
