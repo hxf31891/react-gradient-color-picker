58d31a9d096df15dae376c8ef3ff5f9c0b3fdae3
Thiago Jourdan <thiagojourdan@gmail.com>
2022-09-05 19:24:37 -0300

    :sparkles: Make "gradientParser" accept spaced RGB + alpha colors

--- src/gradientParser.spec.js
+++ src/gradientParser.spec.js
@@ -27,6 +27,19 @@ describe('gradientParser', () => {
     })
   })
 
+  it('should parse linear gradient with rgb + alpha colors', () => {
+    const gradient = 'linear-gradient(45deg, rgb(1 2 3 / 0.123) 0%, rgb(4 5 6 / 0.456) 100%)'
+
+    expect(gradientParser(gradient)).toEqual({
+      colorStops: [
+        {left: 0, value: "RGBA(1, 2, 3, 0.123)"},
+        {left: 100, value: "rgba(4, 5, 6, 0.456)"}
+      ],
+      orientation: {type: "angular", value: "45"},
+      type: "linear-gradient",
+    })
+  })
+
   it('should parse linear gradient with comma-separated rgb colors', () => {
     const gradient = 'linear-gradient(45deg, rgb(1, 2, 3) 0%, rgb(4, 5, 6) 100%)'
 
