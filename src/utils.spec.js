74f5debea06dd5862956012799d88e800dc5d79c
Thiago Jourdan <thiagojourdan@gmail.com>
2022-09-05 16:00:05 -0300

    :umbrella: Add a test for "getGradientType" utility

--- src/utils.spec.js
+++ src/utils.spec.js
@@ -1,4 +1,4 @@
-import {isUpperCase, getNewHsl} from './utils'
+import {isUpperCase, getNewHsl, getGradientType} from './utils'
 
 describe('isUpperCase', () => {
   it('should return true when the first letter of the string is upper-cased', () => {
@@ -23,5 +23,44 @@ describe('getNewHsl', () => {
     getNewHsl(116, 79, 19, 0.5, callback)
 
     expect(callback).toHaveBeenCalledWith(116)
+  })
+})
+
+describe('getGradientType', () => {
+  it('should pick the correct prefix of gradient values', () => {
+    const assertionMap = [
+      ['linear-gradient(30deg, #6789AB 0%, #012345 100%)', 'linear-gradient'],
+      ['-webkit-linear-gradient(30deg, #6789AB 0%, #012345 100%)', '-webkit-linear-gradient'],
+      ['-o-linear-gradient(30deg, #6789AB 0%, #012345 100%)', '-o-linear-gradient'],
+      ['-ms-linear-gradient(30deg, #6789AB 0%, #012345 100%)', '-ms-linear-gradient'],
+      ['-moz-linear-gradient(30deg, #6789AB 0%, #012345 100%)', '-moz-linear-gradient'],
+
+      ['repeating-linear-gradient(30deg, #6789AB 0%, #012345 100%)', 'repeating-linear-gradient'],
+      ['-webkit-repeating-linear-gradient(30deg, #6789AB 0%, #012345 100%)', '-webkit-repeating-linear-gradient'],
+      ['-o-repeating-linear-gradient(30deg, #6789AB 0%, #012345 100%)', '-o-repeating-linear-gradient'],
+      ['-ms-repeating-linear-gradient(30deg, #6789AB 0%, #012345 100%)', '-ms-repeating-linear-gradient'],
+      ['-moz-repeating-linear-gradient(30deg, #6789AB 0%, #012345 100%)', '-moz-repeating-linear-gradient'],
+
+      ['radial-gradient(30deg, #6789AB 0%, #012345 100%)', 'radial-gradient'],
+      ['-webkit-radial-gradient(30deg, #6789AB 0%, #012345 100%)', '-webkit-radial-gradient'],
+      ['-o-radial-gradient(30deg, #6789AB 0%, #012345 100%)', '-o-radial-gradient'],
+      ['-ms-radial-gradient(30deg, #6789AB 0%, #012345 100%)', '-ms-radial-gradient'],
+      ['-moz-radial-gradient(30deg, #6789AB 0%, #012345 100%)', '-moz-radial-gradient'],
+
+      ['repeating-radial-gradient(30deg, #6789AB 0%, #012345 100%)', 'repeating-radial-gradient'],
+      ['-webkit-repeating-radial-gradient(30deg, #6789AB 0%, #012345 100%)', '-webkit-repeating-radial-gradient'],
+      ['-o-repeating-radial-gradient(30deg, #6789AB 0%, #012345 100%)', '-o-repeating-radial-gradient'],
+      ['-ms-repeating-radial-gradient(30deg, #6789AB 0%, #012345 100%)', '-ms-repeating-radial-gradient'],
+      ['-moz-repeating-radial-gradient(30deg, #6789AB 0%, #012345 100%)', '-moz-repeating-radial-gradient'],
+    ]
+    const outputs = [];
+    const expected = [];
+
+    assertionMap.forEach(([value, expectedValue]) => {
+      outputs.push(getGradientType(value))
+      expected.push(expectedValue)
+    })
+
+    expect(outputs).toEqual(expected)
   })
 })
